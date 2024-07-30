918667c5850518d98b34d616e596a1eee7291609
Adrian Maycon <48694649+adrianmaycon@users.noreply.github.com>
2024-07-30 18:28:12 -0300

    Update script.js

--- community-library/js/script.js
+++ community-library/js/script.js
@@ -3,73 +3,85 @@ const books = [
     imgSrc: 'https://m.media-amazon.com/images/I/51XULadddlL.jpg',
     title: 'Dom Quixote',
     author: 'Miguel de Cervantes',
-    description: 'Uma das obras mais importantes da literatura espanhola e mundial, que narra as aventuras do cavaleiro idealista Dom Quixote e seu fiel escudeiro Sancho Pança.'
+    description: 'Uma das obras mais importantes da literatura espanhola e mundial, que narra as aventuras do cavaleiro idealista Dom Quixote e seu fiel escudeiro Sancho Pança.',
+    resume: "A história do engenhoso fidalgo Dom Quixote e de seu fiel escudeiro Sancho Pança conquista leitores geração após geração. O clássico de Miguel de Cervantes é considerado o expoente máximo da literatura espanhola e, em 2002, foi eleito por uma comissão de escritores de 54 países o melhor livro de ficção de todos os tempos. Em homenagem aos 400 anos de morte de Miguel de Cervantes, a Nova Fronteira traz ao público esta edição especial, com a obra integral em dois volumes. O texto de Cervantes é acompanhado das belíssimas ilustrações do francês Gustave Doré, um dos mais fantásticos artistas do século XIX."
   },
   {
     imgSrc: 'https://m.media-amazon.com/images/I/71Xta4Nf7uL._AC_UF1000,1000_QL80_.jpg',
     title: 'Orgulho e Preconceito',
     author: 'Jane Austen',
-    description: 'Um clássico da literatura inglesa que aborda o romance entre Elizabeth Bennet e o arrogante Sr. Darcy, enquanto exploram questões de classe social e valores morais.'
+    description: 'Um clássico da literatura inglesa que aborda o romance entre Elizabeth Bennet e o arrogante Sr. Darcy, enquanto exploram questões de classe social e valores morais.',
+    resume: "A história do engenhoso fidalgo Dom Quixote e de seu fiel escudeiro Sancho Pança conquista leitores geração após geração. O clássico de Miguel de Cervantes é considerado o expoente máximo da literatura espanhola e, em 2002, foi eleito por uma comissão de escritores de 54 países o melhor livro de ficção de todos os tempos. Em homenagem aos 400 anos de morte de Miguel de Cervantes, a Nova Fronteira traz ao público esta edição especial, com a obra integral em dois volumes. O texto de Cervantes é acompanhado das belíssimas ilustrações do francês Gustave Doré, um dos mais fantásticos artistas do século XIX."
   },
   {
     imgSrc: 'https://m.media-amazon.com/images/I/51U-1ilSDEL._AC_UF1000,1000_QL80_.jpg',
     title: 'Cem Anos de Solidão',
     author: 'Gabriel García Márquez',
-    description: 'Um épico da literatura latino-americana que narra a história da família Buendía ao longo de sete gerações na fictícia cidade de Macondo.'
+    description: 'Um épico da literatura latino-americana que narra a história da família Buendía ao longo de sete gerações na fictícia cidade de Macondo.',
+    resume: "A história do engenhoso fidalgo Dom Quixote e de seu fiel escudeiro Sancho Pança conquista leitores geração após geração. O clássico de Miguel de Cervantes é considerado o expoente máximo da literatura espanhola e, em 2002, foi eleito por uma comissão de escritores de 54 países o melhor livro de ficção de todos os tempos. Em homenagem aos 400 anos de morte de Miguel de Cervantes, a Nova Fronteira traz ao público esta edição especial, com a obra integral em dois volumes. O texto de Cervantes é acompanhado das belíssimas ilustrações do francês Gustave Doré, um dos mais fantásticos artistas do século XIX."
   },
   {
     imgSrc: 'https://m.media-amazon.com/images/I/61t0bwt1s3L._AC_UF1000,1000_QL80_.jpg',
     title: '1984',
     author: 'George Orwell',
-    description: 'Um romance distópico que descreve um regime totalitário onde a liberdade individual é suprimida e a realidade é manipulada pelo governo.'
+    description: 'Um romance distópico que descreve um regime totalitário onde a liberdade individual é suprimida e a realidade é manipulada pelo governo.',
+    resume: "A história do engenhoso fidalgo Dom Quixote e de seu fiel escudeiro Sancho Pança conquista leitores geração após geração. O clássico de Miguel de Cervantes é considerado o expoente máximo da literatura espanhola e, em 2002, foi eleito por uma comissão de escritores de 54 países o melhor livro de ficção de todos os tempos. Em homenagem aos 400 anos de morte de Miguel de Cervantes, a Nova Fronteira traz ao público esta edição especial, com a obra integral em dois volumes. O texto de Cervantes é acompanhado das belíssimas ilustrações do francês Gustave Doré, um dos mais fantásticos artistas do século XIX."
   },
   {
     imgSrc: 'https://m.media-amazon.com/images/I/61ATa0Pc4AL._AC_UF1000,1000_QL80_.jpg',
     title: 'O Pequeno Príncipe',
     author: 'Antoine de Saint-Exupéry',
-    description: 'Uma fábula filosófica sobre um príncipe que viaja por diferentes planetas, encontrando personagens peculiares que refletem aspectos da sociedade humana.'
+    description: 'Uma fábula filosófica sobre um príncipe que viaja por diferentes planetas, encontrando personagens peculiares que refletem aspectos da sociedade humana.',
+    resume: "A história do engenhoso fidalgo Dom Quixote e de seu fiel escudeiro Sancho Pança conquista leitores geração após geração. O clássico de Miguel de Cervantes é considerado o expoente máximo da literatura espanhola e, em 2002, foi eleito por uma comissão de escritores de 54 países o melhor livro de ficção de todos os tempos. Em homenagem aos 400 anos de morte de Miguel de Cervantes, a Nova Fronteira traz ao público esta edição especial, com a obra integral em dois volumes. O texto de Cervantes é acompanhado das belíssimas ilustrações do francês Gustave Doré, um dos mais fantásticos artistas do século XIX."
   },
   {
     imgSrc: 'https://m.media-amazon.com/images/I/71ZLavBjpRL._AC_UF1000,1000_QL80_.jpg',
     title: 'O Senhor dos Anéis',
     author: 'J.R.R. Tolkien',
-    description: 'Uma trilogia de fantasia épica que segue a jornada do hobbit Frodo Bolsa de Bolseiro para destruir um anel mágico e derrotar o mal em Terra-média.'
+    description: 'Uma trilogia de fantasia épica que segue a jornada do hobbit Frodo Bolsa de Bolseiro para destruir um anel mágico e derrotar o mal em Terra-média.',
+    resume: "A história do engenhoso fidalgo Dom Quixote e de seu fiel escudeiro Sancho Pança conquista leitores geração após geração. O clássico de Miguel de Cervantes é considerado o expoente máximo da literatura espanhola e, em 2002, foi eleito por uma comissão de escritores de 54 países o melhor livro de ficção de todos os tempos. Em homenagem aos 400 anos de morte de Miguel de Cervantes, a Nova Fronteira traz ao público esta edição especial, com a obra integral em dois volumes. O texto de Cervantes é acompanhado das belíssimas ilustrações do francês Gustave Doré, um dos mais fantásticos artistas do século XIX."
   },
   {
     imgSrc: 'https://m.media-amazon.com/images/I/81+dpv22LGL._AC_UF1000,1000_QL80_.jpg',
     title: 'Crime e Castigo',
     author: 'Fiódor Dostoiévski',
-    description: 'Um romance psicológico que explora as complexidades da moralidade e do crime através da história de Raskólnikov, um estudante atormentado por sua própria consciência.'
+    description: 'Um romance psicológico que explora as complexidades da moralidade e do crime através da história de Raskólnikov, um estudante atormentado por sua própria consciência.',
+    resume: "A história do engenhoso fidalgo Dom Quixote e de seu fiel escudeiro Sancho Pança conquista leitores geração após geração. O clássico de Miguel de Cervantes é considerado o expoente máximo da literatura espanhola e, em 2002, foi eleito por uma comissão de escritores de 54 países o melhor livro de ficção de todos os tempos. Em homenagem aos 400 anos de morte de Miguel de Cervantes, a Nova Fronteira traz ao público esta edição especial, com a obra integral em dois volumes. O texto de Cervantes é acompanhado das belíssimas ilustrações do francês Gustave Doré, um dos mais fantásticos artistas do século XIX."
   },
   {
     imgSrc: 'https://m.media-amazon.com/images/I/71mFnG3Bn3L._AC_UF1000,1000_QL80_.jpg',
     title: 'A Metamorfose',
     author: 'Franz Kafka',
-    description: 'Uma novela surrealista que conta a história de Gregor Samsa, um homem que se transforma em um inseto e enfrenta o isolamento e a alienação de sua família.'
+    description: 'Uma novela surrealista que conta a história de Gregor Samsa, um homem que se transforma em um inseto e enfrenta o isolamento e a alienação de sua família.',
+    resume: "A história do engenhoso fidalgo Dom Quixote e de seu fiel escudeiro Sancho Pança conquista leitores geração após geração. O clássico de Miguel de Cervantes é considerado o expoente máximo da literatura espanhola e, em 2002, foi eleito por uma comissão de escritores de 54 países o melhor livro de ficção de todos os tempos. Em homenagem aos 400 anos de morte de Miguel de Cervantes, a Nova Fronteira traz ao público esta edição especial, com a obra integral em dois volumes. O texto de Cervantes é acompanhado das belíssimas ilustrações do francês Gustave Doré, um dos mais fantásticos artistas do século XIX."
   },
   {
     imgSrc: 'https://m.media-amazon.com/images/I/91BsZhxCRjL._AC_UF1000,1000_QL80_.jpg',
     title: 'A Revolução dos Bichos',
     author: 'George Orwell',
-    description: 'Uma sátira política que retrata uma fazenda onde os animais expulsam seus donos humanos e tentam estabelecer uma sociedade igualitária, que eventualmente degenera em tirania.'
+    description: 'Uma sátira política que retrata uma fazenda onde os animais expulsam seus donos humanos e tentam estabelecer uma sociedade igualitária, que eventualmente degenera em tirania.',
+    resume: "A história do engenhoso fidalgo Dom Quixote e de seu fiel escudeiro Sancho Pança conquista leitores geração após geração. O clássico de Miguel de Cervantes é considerado o expoente máximo da literatura espanhola e, em 2002, foi eleito por uma comissão de escritores de 54 países o melhor livro de ficção de todos os tempos. Em homenagem aos 400 anos de morte de Miguel de Cervantes, a Nova Fronteira traz ao público esta edição especial, com a obra integral em dois volumes. O texto de Cervantes é acompanhado das belíssimas ilustrações do francês Gustave Doré, um dos mais fantásticos artistas do século XIX."
   },
   {
     imgSrc: 'https://m.media-amazon.com/images/I/91K-MrUTNOL._AC_UF1000,1000_QL80_.jpg',
     title: 'A Culpa é das Estrelas',
     author: 'John Green',
-    description: 'Um romance contemporâneo que narra a história de Hazel Grace Lancaster, uma jovem com câncer, e Augustus Waters, um ex-jogador de basquete, que se apaixonam enquanto enfrentam suas doenças.'
+    description: 'Um romance contemporâneo que narra a história de Hazel Grace Lancaster, uma jovem com câncer, e Augustus Waters, um ex-jogador de basquete, que se apaixonam enquanto enfrentam suas doenças.',
+    resume: "A história do engenhoso fidalgo Dom Quixote e de seu fiel escudeiro Sancho Pança conquista leitores geração após geração. O clássico de Miguel de Cervantes é considerado o expoente máximo da literatura espanhola e, em 2002, foi eleito por uma comissão de escritores de 54 países o melhor livro de ficção de todos os tempos. Em homenagem aos 400 anos de morte de Miguel de Cervantes, a Nova Fronteira traz ao público esta edição especial, com a obra integral em dois volumes. O texto de Cervantes é acompanhado das belíssimas ilustrações do francês Gustave Doré, um dos mais fantásticos artistas do século XIX."
   },
   {
     imgSrc: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKq8IwSFaD37SR9lekMV_rZLoadAJT20bGdg&s',
     title: 'Memórias Póstumas de Brás Cubas',
     author: 'Machado de Assis',
-    description: 'Um romance brasileiro que é uma sátira mordaz à sociedade brasileira do século XIX, narrado por um defunto autor que reflete sobre sua vida e suas imperfeições.'
+    description: 'Um romance brasileiro que é uma sátira mordaz à sociedade brasileira do século XIX, narrado por um defunto autor que reflete sobre sua vida e suas imperfeições.',
+    resume: "A história do engenhoso fidalgo Dom Quixote e de seu fiel escudeiro Sancho Pança conquista leitores geração após geração. O clássico de Miguel de Cervantes é considerado o expoente máximo da literatura espanhola e, em 2002, foi eleito por uma comissão de escritores de 54 países o melhor livro de ficção de todos os tempos. Em homenagem aos 400 anos de morte de Miguel de Cervantes, a Nova Fronteira traz ao público esta edição especial, com a obra integral em dois volumes. O texto de Cervantes é acompanhado das belíssimas ilustrações do francês Gustave Doré, um dos mais fantásticos artistas do século XIX."
   },
   {
     imgSrc: 'https://m.media-amazon.com/images/I/81ibfYk4qmL._AC_UF1000,1000_QL80_.jpg',
     title: 'Harry Potter e a Pedra Filosofal',
     author: 'J.K. Rowling',
-    description: 'O primeiro livro da série Harry Potter, que segue as aventuras de um jovem bruxo, Harry, e seus amigos Hermione Granger e Ron Weasley enquanto frequentam a escola de magia de Hogwarts.'
+    description: 'O primeiro livro da série Harry Potter, que segue as aventuras de um jovem bruxo, Harry, e seus amigos Hermione Granger e Ron Weasley enquanto frequentam a escola de magia de Hogwarts.',
+    resume: "A história do engenhoso fidalgo Dom Quixote e de seu fiel escudeiro Sancho Pança conquista leitores geração após geração. O clássico de Miguel de Cervantes é considerado o expoente máximo da literatura espanhola e, em 2002, foi eleito por uma comissão de escritores de 54 países o melhor livro de ficção de todos os tempos. Em homenagem aos 400 anos de morte de Miguel de Cervantes, a Nova Fronteira traz ao público esta edição especial, com a obra integral em dois volumes. O texto de Cervantes é acompanhado das belíssimas ilustrações do francês Gustave Doré, um dos mais fantásticos artistas do século XIX."
   },
 ];
 
@@ -100,7 +112,7 @@ function viewDataForm() {
   const form = document.getElementById('contactForm');
   const formMessage = document.getElementById('formMessage');
 
-  form.addEventListener('submit', function(event) {
+  form.addEventListener('submit', function (event) {
     event.preventDefault(); // Evita o envio padrão do formulário
 
     // Captura os dados do formulário
@@ -127,22 +139,22 @@ function getQueryParam(param) {
   return urlParams.get(param);
 }
 
-function addDetailsBook() {  
-// Obtém o título do livro da Url
-const bookId = getQueryParam('bookId');
+function addDetailsBook() {
+  // Obtém o título do livro da Url
+  const bookId = getQueryParam('bookId');
 
-// Encontrar o livro correspondente ao bookId
-const bookData = books.find(book => book.title === bookId);
+  // Encontrar o livro correspondente ao bookId
+  const bookData = books.find(book => book.title === bookId);
 
-// Exibir os detalhes do livro
-if (bookData) {
-  document.getElementById('bookImage').src = bookData.imgSrc;
-  document.getElementById('bookTitle').textContent = bookData.title;
-  document.getElementById('bookAuthor').textContent = bookData.author;
-  document.getElementById('bookDescription').textContent = bookData.description;
-} else {
-  document.getElementById('bookTitle').textContent = "Livro não encontrado";
-}
+  // Exibir os detalhes do livro
+  if (bookData) {
+    document.getElementById('bookImage').src = bookData.imgSrc;
+    document.getElementById('bookTitle').textContent = `Livro: ${bookData.title}`;
+    document.getElementById('bookAuthor').textContent = `Autor: ${bookData.author}`;
+    document.getElementById('bookDescription').textContent = `Resumo: ${bookData.resume}`;
+  } else {
+    document.getElementById('bookTitle').textContent = "Livro não encontrado";
+  }
 }
 
 document.addEventListener('DOMContentLoaded', addBooksToPage);
